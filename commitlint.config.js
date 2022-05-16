df2332eeefa8c2ae06709ff5aad119ca9dd8e902
Namee2810 <dvn281002@gmail.com>
2022-05-16 19:55:22 +0700

    improve: commit lint setting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "docs",
+        "feature",
+        "fix",
+        "improve",
+        "perform",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
 }
